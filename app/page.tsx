b1e3ec189a9e8aa73e447c274f7bb351622df13d
Rafael Caslu Henry <rafaelhenrycassiano23@gmail.com>
2024-08-06 23:40:17 -0300

    feat: added the Header.tsx file and installed the card from shadowcn library

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 "use client"
 
-import exp from "constants";
+import exp from "constants"
 import { useState } from "react"
-import { Button } from "./_components/ui/button";
 
 const Home = () => {
-  const [] = useState();
-  return <Button>teste</Button>
-};
+  const [] = useState()
+  return <div></div>
+}
 
-export default Home;                        
+export default Home
