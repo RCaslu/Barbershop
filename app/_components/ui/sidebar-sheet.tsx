a65641cb3c63a53e6f19b2e867edf297034e66a3
Rafael Caslu Henry <rafaelhenrycassiano23@gmail.com>
2024-08-09 21:47:57 -0300

    feat: started to add the google sync

--- app/_components/ui/sidebar-sheet.tsx
+++ app/_components/ui/sidebar-sheet.tsx
@@ -2,10 +2,25 @@ import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./sheet"
 import { quickSearchOptions } from "@/app/_constants/search"
 
 import { Button } from "./button"
-import { HomeIcon, CalendarIcon, LogOutIcon, MenuIcon } from "lucide-react"
+import {
+  HomeIcon,
+  CalendarIcon,
+  LogOutIcon,
+  MenuIcon,
+  LogInIcon,
+  Icon,
+} from "lucide-react"
 import Link from "next/link"
 import Image from "next/image"
 import { Avatar, AvatarImage } from "./avatar"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./dialog"
 
 const SidebarSheets = () => {
   return (
@@ -14,8 +29,33 @@ const SidebarSheets = () => {
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
 
-      <div className="flex items-center gap-3 border-b border-solid py-5">
-        <Avatar>
+      <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
+        <h2 className="font-bold">Olá, faça seu login!</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="icon">
+              <LogInIcon />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[90%]">
+            <DialogHeader>
+              <DialogTitle>Faça login na plataforma</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando sua conta do Google.
+              </DialogDescription>
+            </DialogHeader>
+            <Button variant={"outline"} className="gap-2 font-bold">
+              <Image
+                src="/google.png"
+                alt="Fazer login com o goole"
+                width={18}
+                height={18}
+              />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+        {/*  <Avatar>
           <AvatarImage
             src={
               "https://plus.unsplash.com/premium_vector-1713940500816-683e3727723f?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -26,7 +66,7 @@ const SidebarSheets = () => {
         <div>
           <p className="font-bold">Rafael</p>
           <p className="text-xs">rafaelhenry@gmail.com</p>
-        </div>
+        </div> */}
       </div>
 
       <div className="flex flex-col gap-2 border-b border-solid py-5">
